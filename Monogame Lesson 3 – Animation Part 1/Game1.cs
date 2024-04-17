a59f932035fd222ede82cafdd4aae6b7ab1cef0b
Chickenmist <wpimlatt@gmail.com>
2024-04-17 14:42:32 -0400

    Tribble grey and brown done Work on tribble orange started

--- Monogame Lesson 3 – Animation Part 1/Game1.cs
+++ Monogame Lesson 3 – Animation Part 1/Game1.cs
@@ -10,7 +10,7 @@ namespace Monogame_Lesson_3___Animation_Part_1
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Random _random;
+        Random _random = new Random();
 
         //Gray tribble variables
         Texture2D tribbleGreyTexture;
@@ -22,6 +22,18 @@ namespace Monogame_Lesson_3___Animation_Part_1
         Texture2D tribbleBrownTexture;
         Rectangle tribbleBrownRect;
         Vector2 tribbleBrownSpeed;
+        //
+
+        //Orange tribble variables
+        Texture2D tribbleOrangeTexture;
+        Rectangle tribbleOrangeRect;
+        Vector2 tribbleOrangeSpeed;
+        //
+
+        //Cream tribble variables
+        Texture2D tribbleCreamTexture;
+        Rectangle tribbleCreamRect;
+        Vector2 tribbleCreamSpeed;
         //
 
         public Game1()
@@ -41,14 +53,24 @@ namespace Monogame_Lesson_3___Animation_Part_1
 
             //Gray tribble 
             tribbleGreyTexture = Content.Load<Texture2D>("tribbleGrey");
-            tribbleGreyRect = new Rectangle(300, 10, 100, 100);
+            tribbleGreyRect = new Rectangle(_random.Next(0, _graphics.PreferredBackBufferWidth - 100), _random.Next(0, _graphics.PreferredBackBufferHeight - 100), 100, 100);
             tribbleGreySpeed = new Vector2(2, 2);
             //
 
             //Brown tribble
             tribbleBrownTexture = Content.Load<Texture2D>("tribbleBrown");
-            tribbleBrownRect = new Rectangle(300, 10, 100, 100);
+            tribbleBrownRect = new Rectangle(_random.Next(0, _graphics.PreferredBackBufferWidth - 100), _random.Next(0, _graphics.PreferredBackBufferHeight - 100), 100, 100);
             tribbleBrownSpeed = new Vector2(2, 0);
+            //
+
+            //Orange tribble
+            tribbleOrangeTexture = Content.Load<Texture2D>("tribbleOrange");
+            tribbleOrangeRect = new Rectangle(_random.Next(0, _graphics.PreferredBackBufferWidth - 100), _random.Next(0, _graphics.PreferredBackBufferHeight - 100), 100, 100);
+            tribbleOrangeSpeed = new Vector2(2, 2);
+            //
+
+            //Cream tribble
+
             //
 
             base.Initialize();
@@ -71,25 +93,62 @@ namespace Monogame_Lesson_3___Animation_Part_1
             // Gray tribble
             tribbleGreyRect.X += (int)tribbleGreySpeed.X;
             tribbleGreyRect.Y += (int)tribbleGreySpeed.Y;
-            if (tribbleGreyRect.Right > _graphics.PreferredBackBufferWidth || tribbleGreyRect.Left < 0)
+            if (tribbleGreyRect.Right > _graphics.PreferredBackBufferWidth)
             {
-                tribbleGreySpeed.X *= -1;
+                tribbleGreyRect.X = _graphics.PreferredBackBufferWidth - tribbleBrownRect.Width;
+
+                tribbleGreySpeed = new Vector2(_random.Next(-15, 0), _random.Next(-15, 16));
+            }
+            else if (tribbleGreyRect.Left < 0)
+            {
+                tribbleGreyRect.X = 0;
+
+                tribbleGreySpeed = new Vector2(_random.Next(1, 16), _random.Next(-15, 16));
             }
 
-            if (tribbleGreyRect.Bottom > _graphics.PreferredBackBufferHeight || tribbleGreyRect.Top < 0)
+            if (tribbleGreyRect.Bottom > _graphics.PreferredBackBufferHeight)
             {
-                tribbleGreySpeed.Y *= -1;
+                tribbleGreyRect.Y = _graphics.PreferredBackBufferHeight - tribbleBrownRect.Height;
+
+                tribbleGreySpeed = new Vector2(_random.Next(-15, 16), _random.Next(-15, 0));
+            }
+            else if (tribbleGreyRect.Top < 0)
+            {
+                tribbleGreyRect.Y = 0;
+
+                tribbleGreySpeed = new Vector2(_random.Next(-15, 16), _random.Next(1, 16));
             }
             //
 
             //Brown tribble
             tribbleBrownRect.X += (int)tribbleBrownSpeed.X;
-            tribbleBrownRect.Y += (int)tribbleBrownSpeed.Y;
-            if (tribbleBrownRect.Right > _graphics.PreferredBackBufferWidth || tribbleBrownRect.Left < 0)
+            if (tribbleBrownRect.Right > _graphics.PreferredBackBufferWidth)
             {
-                tribbleBrownSpeed.X *= -1;
+                tribbleBrownRect.X = _graphics.PreferredBackBufferWidth - tribbleBrownRect.Width;
+                tribbleBrownSpeed = new Vector2(_random.Next(-16, 0), 0);
+            }
+            else if (tribbleBrownRect.Left < 0)
+            {
+                tribbleBrownRect.X = 0;
+                tribbleBrownSpeed = new Vector2(_random.Next(1, 16), 0);
+            }
+
+            //
+
+            //Orange tribble
+            tribbleOrangeRect.Offset(tribbleOrangeSpeed);
+            if (tribbleOrangeRect.Right > _graphics.PreferredBackBufferWidth || tribbleOrangeRect.Left < 0)
+            {
+                tribbleOrangeSpeed.X *= -2;
+            }
+
+            if (tribbleOrangeRect.Bottom > _graphics.PreferredBackBufferHeight || tribbleOrangeRect.Top < 0)
+            {
+                tribbleOrangeSpeed.Y *= -2;
             }
             //
+
+            //Cream tribble
 
             base.Update(gameTime);
         }
@@ -104,6 +163,8 @@ namespace Monogame_Lesson_3___Animation_Part_1
             
             _spriteBatch.Draw(tribbleGreyTexture, tribbleGreyRect, Color.White);
             _spriteBatch.Draw(tribbleBrownTexture, tribbleBrownRect, Color.White);
+
+            _spriteBatch.Draw(tribbleOrangeTexture, tribbleOrangeRect, Color.White);
 
             _spriteBatch.End();
 
